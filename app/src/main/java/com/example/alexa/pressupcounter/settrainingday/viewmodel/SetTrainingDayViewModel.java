d0706761927b8cdcd5844243b367e7bc0a2d5e8f
Alexandr <alexandr.agress1on@gmail.com>
2019-03-14 15:44:09 +0300

    - сделал разметку для SetTrainingDay и выбор дней

--- app/src/main/java/com/example/alexa/pressupcounter/settrainingday/viewmodel/SetTrainingDayViewModel.java
+++ app/src/main/java/com/example/alexa/pressupcounter/settrainingday/viewmodel/SetTrainingDayViewModel.java
@@ -1,9 +1,27 @@
 package com.example.alexa.pressupcounter.settrainingday.viewmodel;
 
+import androidx.databinding.ObservableField;
+
 /**
  * Created by Alexandr Mikhalev on 13.03.2019.
  *
  * @author Alexandr Mikhalev
  */
 public interface SetTrainingDayViewModel {
+
+    ObservableField<Boolean> getMonday();
+
+    ObservableField<Boolean> getTuesday();
+
+    ObservableField<Boolean> getWednesday();
+
+    ObservableField<Boolean> getThursday();
+
+    ObservableField<Boolean> getFriday();
+
+    ObservableField<Boolean> getSaturday();
+
+    ObservableField<Boolean> getSunday();
+
+    void onCheckedChanged(int i, boolean state);
 }
--- app/src/main/java/com/example/alexa/pressupcounter/settrainingday/viewmodel/SetTrainingDayViewModelImpl.java
+++ app/src/main/java/com/example/alexa/pressupcounter/settrainingday/viewmodel/SetTrainingDayViewModelImpl.java
@@ -2,6 +2,7 @@ package com.example.alexa.pressupcounter.settrainingday.viewmodel;
 
 import com.example.alexa.pressupcounter.settrainingday.model.SetTrainingDayModel;
 
+import androidx.databinding.ObservableField;
 import androidx.lifecycle.ViewModel;
 
 /**
@@ -13,7 +14,71 @@ public class SetTrainingDayViewModelImpl extends ViewModel implements SetTrainin
 
     private SetTrainingDayModel mSetTrainingDayModel;
 
+    private ObservableField<Boolean> mMonday;
+    private ObservableField<Boolean> mTuesday;
+    private ObservableField<Boolean> mWednesday;
+    private ObservableField<Boolean> mThursday;
+    private ObservableField<Boolean> mFriday;
+    private ObservableField<Boolean> mSaturday;
+    private ObservableField<Boolean> mSunday;
+
     public SetTrainingDayViewModelImpl(SetTrainingDayModel setTrainingDayModel) {
         mSetTrainingDayModel = setTrainingDayModel;
+
+        mMonday = new ObservableField<>(false);
+        mTuesday = new ObservableField<>(false);
+        mWednesday= new ObservableField<>(false);
+        mThursday = new ObservableField<>(false);
+        mFriday = new ObservableField<>(false);
+        mSaturday = new ObservableField<>(false);
+        mSunday = new ObservableField<>(false);
+    }
+
+    @Override
+    public ObservableField<Boolean> getMonday() {
+        return mMonday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getTuesday() {
+        return mTuesday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getWednesday() {
+        return mWednesday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getThursday() {
+        return mThursday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getFriday() {
+        return mFriday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getSaturday() {
+        return mSaturday;
+    }
+
+    @Override
+    public ObservableField<Boolean> getSunday() {
+        return mSunday;
+    }
+
+    @Override
+    public void onCheckedChanged(int i, boolean state) {
+        switch (i) {
+            case 1: mMonday.set(state); break;
+            case 2: mTuesday.set(state) ;break;
+            case 3: mWednesday.set(state); break;
+            case 4: mThursday.set(state); break;
+            case 5: mFriday.set(state); break;
+            case 6: mSaturday.set(state); break;
+            case 7: mSunday.set(state); break;
+        }
     }
 }
