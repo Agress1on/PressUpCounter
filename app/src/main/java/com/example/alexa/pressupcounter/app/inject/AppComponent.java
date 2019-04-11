727141ead3cec2fdd8233acec56b7c9763727b7c
Alexandr <alexandr.agress1on@gmail.com>
2019-04-11 17:06:20 +0300

    - доставил safeUnbox в разметке

--- app/src/main/java/com/example/alexa/pressupcounter/app/inject/AppComponent.java
+++ app/src/main/java/com/example/alexa/pressupcounter/app/inject/AppComponent.java
@@ -3,22 +3,6 @@ package com.example.alexa.pressupcounter.app.inject;
 import android.content.Context;
 
 import com.example.alexa.pressupcounter.app.App;
-import com.example.alexa.pressupcounter.firstlaunch.inject.FirstLaunchComponent;
-import com.example.alexa.pressupcounter.firstlaunch.inject.FirstLaunchModule;
-import com.example.alexa.pressupcounter.resulttraining.inject.ResultTrainingComponent;
-import com.example.alexa.pressupcounter.resulttraining.inject.ResultTrainingModule;
-import com.example.alexa.pressupcounter.setprogram.inject.SetProgramComponent;
-import com.example.alexa.pressupcounter.setprogram.inject.SetProgramModule;
-import com.example.alexa.pressupcounter.settime.inject.SetTimeComponent;
-import com.example.alexa.pressupcounter.settime.inject.SetTimeModule;
-import com.example.alexa.pressupcounter.settings.inject.SettingsComponent;
-import com.example.alexa.pressupcounter.settings.inject.SettingsModule;
-import com.example.alexa.pressupcounter.settrainingday.inject.SetTrainingDayComponent;
-import com.example.alexa.pressupcounter.settrainingday.inject.SetTrainingDayModule;
-import com.example.alexa.pressupcounter.starttraining.inject.StartTrainingComponent;
-import com.example.alexa.pressupcounter.starttraining.inject.StartTrainingModule;
-import com.example.alexa.pressupcounter.traininglist.inject.TrainingListComponent;
-import com.example.alexa.pressupcounter.traininglist.inject.TrainingListModule;
 
 import dagger.BindsInstance;
 import dagger.Component;
